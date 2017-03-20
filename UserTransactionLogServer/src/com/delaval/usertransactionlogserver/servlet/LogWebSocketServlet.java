259090f57ead5baa0bbc0dc40a8355f607581934
engmbe <engmbe@403474d4-8537-446f-9ecb-d9f2842470be>
2017-03-20 14:18:48 +0000

    Compression of websocket communication
    git-svn-id: svn://10.33.1.15/syseng/trunk/UserTransactionLog@8116 403474d4-8537-446f-9ecb-d9f2842470be

--- UserTransactionLogServer/src/com/delaval/usertransactionlogserver/servlet/LogWebSocketServlet.java
+++ UserTransactionLogServer/src/com/delaval/usertransactionlogserver/servlet/LogWebSocketServlet.java
@@ -3,6 +3,7 @@ package com.delaval.usertransactionlogserver.servlet;
 import com.delaval.usertransactionlogserver.websocket.UserTransactionLogWebSocket;
 import org.eclipse.jetty.websocket.servlet.WebSocketServlet;
 import org.eclipse.jetty.websocket.servlet.WebSocketServletFactory;
+import org.eclipse.jetty.websocket.common.extensions.compress.PerMessageDeflateExtension;
 
 import javax.servlet.annotation.WebServlet;
 
@@ -16,6 +17,8 @@ public class LogWebSocketServlet extends WebSocketServlet {
 
     @Override
     public void configure(WebSocketServletFactory factory) {
+        factory.getExtensionFactory().register(
+                                               "permessage-deflate",PerMessageDeflateExtension.class);
         factory.register(UserTransactionLogWebSocket.class);
     }
 }
